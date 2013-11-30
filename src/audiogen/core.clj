3a03ca0e272cefc936fb5c2f80bb4a676637457b
tehsven <tehsven@gmail.com>
2013-11-30 14:13:48 -0600

    octave agent. print-usage

--- src/audiogen/core.clj
+++ src/audiogen/core.clj
@@ -4,18 +4,21 @@
         overtone.inst.piano)
   (:gen-class))
 
+(def oct (agent 72))
+(defn oct-up [c] (+ c 12))
+(defn oct-down [c] (- c 12))
+
 (def int-to-note {
-  97 72, 119 73, 115 74, 101 75, 
-  100 76, 102 77, 116 78, 103 79, 
-  121 80, 104 81, 117 82, 106 83, 
-  107 84, 111 85, 108 86, 112 87
+  97 #(+ @oct 0), 119 #(+ @oct 1), 115 #(+ @oct 2), 101 #(+ @oct 3), 
+  100 #(+ @oct 4), 102 #(+ @oct 5), 116 #(+ @oct 6), 103 #(+ @oct 7), 
+  121 #(+ @oct 8), 104 #(+ @oct 9), 117 #(+ @oct 10), 106 #(+ @oct 11), 
+  107 #(+ @oct 12), 111 #(+ @oct 13), 108 #(+ @oct 14), 112 #(+ @oct 15)
   })
 
 (defn note-player
   "plays the provided note using the current instrument"
   [note]
-  (piano note)
-  :note-played)
+  (piano note))
 
 (defn is-note?
   "returns if the input key is a note or not"
@@ -29,22 +32,30 @@
   []
   (let [read-key (.readCharacter key-reader)]
     (if (contains? int-to-note read-key)
-      (note-player (int-to-note read-key))
+      (note-player ((int-to-note read-key)))
       (case read-key
         113 :quit ;q
-        :other))))
+        45 (send oct oct-down) ;-
+        61 (send oct oct-up) ;+
+        :default))))
 
 (defn key-convert
   [key-sym]
   (case key-sym
     :quit false
-    :note-played true
-    :other true))
+    true))
+
+(defn print-usage
+  []
+  (println 
+  "start typing for instrument playback!
+  +- : change octave
+  q : quit"))
 
 (defn start
   "convert keystrokes into musical instrument playback"
   []
-  (println "start typing for instrument playback! q to quit")
+  (print-usage)
   (let [loop-fn #(key-convert (key-parse))]
       (while (loop-fn)))
   "thanks for playing!")
