54800f1b800b6d848f765d6bc4140df277f09fee
tehsven <tehsven@gmail.com>
2013-11-30 13:14:51 -0600

    refactoring key parsing to system control

--- src/audiogen/core.clj
+++ src/audiogen/core.clj
@@ -11,15 +11,42 @@
   107 84, 111 85, 108 86, 112 87
   })
 
+(defn note-player
+  "plays the provided note using the current instrument"
+  [note]
+  (piano note)
+  :note-played)
+
+(defn is-note?
+  "returns if the input key is a note or not"
+  [note]
+  (contains? int-to-note note))
+
+(def key-reader (ConsoleReader.))
+
+(defn key-parse
+  "parses a key into a symbol"
+  []
+  (let [read-key (.readCharacter key-reader)]
+    (if (contains? int-to-note read-key)
+      (note-player (int-to-note read-key))
+      (case read-key
+        113 :quit ;q
+        :other))))
+
+(defn key-convert
+  [key-sym]
+  (case key-sym
+    :quit false
+    :note-played true
+    :other true))
+
 (defn start
   "convert keystrokes into musical instrument playback"
   []
   (println "start typing for great musical fun!")
-  (let [keyreader (ConsoleReader.)
-        keynotegrabber #(int-to-note (.readCharacter keyreader))]
-      (loop [keyednote (keynotegrabber)]
-        (if keyednote
-          (do
-            (piano keyednote)
-            (recur (keynotegrabber))))))
-  (println "thanks for playing!"))
+  (let [loop-fn #(key-convert (key-parse))]
+      (loop [continue? (loop-fn)]
+        (if continue?
+            (recur (loop-fn)))))
+  "thanks for playing!")
