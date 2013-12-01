117d212573e5d43c4159b78b292bbb0fb24ea51c
tehsven <tehsven@gmail.com>
2013-12-01 14:46:56 -0600

    fix auto generated readme

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject audiogen "0.1.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "exploring audio generation with clojure"
+  :url "https://github.com/tehsven/audiogen-clojure/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
