5b879e1670c0b6d452a69caab9da3da89a3adc2c
tehsven <tehsven@gmail.com>
2013-12-01 15:24:51 -0600

    cleanup, documentation

--- src/audiogen/drums.clj
+++ src/audiogen/drums.clj
@@ -4,10 +4,10 @@
   (:gen-class))
 
 (def drum-bindings {
-  122 #(kick :amp 1), ;z
-  109 #(snare :amp 1), ;m
+  122 #(kick :amp 1),       ;z
+  109 #(snare :amp 1),      ;m
   110 #(closed-hat :amp 1), ;n
-  98 #(open-hat :amp 1) ;b
+  98 #(open-hat :amp 1)     ;b
   })
 
 (add-bindings drum-bindings)
--- src/audiogen/piano.clj
+++ src/audiogen/piano.clj
@@ -4,12 +4,10 @@
   (:gen-class))
 
 (def oct (agent 72))
-(defn oct-up [c] (+ c 12))
-(defn oct-down [c] (- c 12))
 
 (def oct-bindings {
-  45 #(send oct (fn [c] (- c 12))), ;- lower octave
-  61 #(send oct (fn [c] (+ c 12)))  ;+ raise octave
+  45 #(send oct (fn [c] (- c 12))), ;-
+  61 #(send oct (fn [c] (+ c 12)))  ;+
   })
 
 (defn play
@@ -18,10 +16,22 @@
 	(piano (+ @oct note)))
 
 (def note-bindings {
-  97 #(play 0), 119 #(play 1), 115 #(play 2), 101 #(play 3), 
-  100 #(play 4), 102 #(play 5), 116 #(play 6), 103 #(play 7), 
-  121 #(play 8), 104 #(play 9), 117 #(play 10), 106 #(play 11), 
-  107 #(play 12), 111 #(play 13), 108 #(play 14), 112 #(play 15)
+  97 #(play 0), 	;a
+  119 #(play 1), 	;w
+  115 #(play 2), 	;s
+  101 #(play 3), 	;e
+  100 #(play 4), 	;d
+  102 #(play 5), 	;f
+  116 #(play 6), 	;t
+  103 #(play 7), 	;g
+  121 #(play 8), 	;y
+  104 #(play 9), 	;h
+  117 #(play 10), 	;u
+  106 #(play 11), 	;j
+  107 #(play 12), 	;k
+  111 #(play 13), 	;o
+  108 #(play 14), 	;l
+  112 #(play 15) 	;p
   })
 
 (add-bindings oct-bindings)
