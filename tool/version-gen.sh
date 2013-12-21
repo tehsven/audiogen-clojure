572d9039445ed5f7aba305d3e55cbce60835c31c
tehsven <tehsven@tehsvens-Mac-Pro.local>
2013-12-20 20:15:03 -0600

    no more brew clojure

--- tool/version-gen.sh
+++ tool/version-gen.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "brew :" `brew --version`
 git --version | sed 's/version/:/'
-brew info clojure | grep clojure: | cut -d \  -f 1,3 | sed 's/e:/e :/' | sed 's/,//'
 lein --version | echo "lein :" `cut -d \  -f 2`
