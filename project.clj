f0639927006ad70fe11764fdc5f25a62f4a3f962
tehsven <tehsven@gmail.com>
2013-12-31 09:11:55 -0600

    update milestone

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject audiogen "0.1.3"
+(defproject audiogen "0.1.4"
   :description "exploring audio generation with clojure"
   :url "https://github.com/tehsven/audiogen-clojure/"
   :license {:name "Eclipse Public License"
