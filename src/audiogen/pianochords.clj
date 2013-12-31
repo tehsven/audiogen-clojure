a98447ac104d0b0457cdb428f271f297960317cc
tehsven <tehsven@gmail.com>
2013-12-30 20:01:08 -0600

    build chords based on scale degree instead of hardcode

--- src/audiogen/pianochords.clj
+++ src/audiogen/pianochords.clj
@@ -3,25 +3,26 @@
         overtone.music.pitch)
   (:gen-class))
 
+(def degrees '(:i :ii :iii :iv :v :vi :vii))
 (def row-1 '(\1 \2 \3 \4 \5 \6 \7 \8 \9 \0))
 (def row-q '(\q \w \e \r \t \y \u \i \o \p))
 (def row-w '(\a \s \d \f \g \h \j \k \l))
 (def row-z '(\z \x \c \v \b \n \m))
 
 (defn build-chord-scale
-  [root chord-name scale-name]
+  [root scale-name]
   (into {}
-    (map #(vector %1 (chord %2 chord-name))
+    (map #(vector %1 (chord-degree %2 root scale-name 3))
       row-w
-      (scale root scale-name))))
+      degrees)))
 
 (def mappings
-  (agent (build-chord-scale :c4 :major :major)))
+  (agent (build-chord-scale :c4 :major)))
 
 (defn use-chord-scale
-  [root scale-name chord-name]
+  [root scale-name]
   (send mappings (fn [c]
-    (build-chord-scale root chord-name scale-name))))
+    (build-chord-scale root scale-name))))
 
 (defn play-chord
 	"plays chords"
@@ -35,7 +36,7 @@
       (map #(vector %1 (fn [] (play-chord (@mappings %1))))
         row-w))
     {
-      \1 #(use-chord-scale :c4 :major :major)
-      \2 #(use-chord-scale :d4 :minor :major)
-      \3 #(use-chord-scale :f4 :locrian :minor)
+      \1 #(use-chord-scale :c4 :major)
+      \2 #(use-chord-scale :d4 :minor)
+      \3 #(use-chord-scale :f4 :locrian)
     }))
